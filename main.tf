99035f2d168316d10f63260c71eb03c1798ef883
Yue Yuan <y.b.yuan@accenture.com>
2022-01-06 22:31:30 +0100

    aa

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.70.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-099ccc441b2ef41ec"
+  instance_type = "t2.micro"
+
+
+
+}
